bd6a740b1bc1937df7f8e6028ea098d9adec087c
railprophet <rbdmit@gmail.com>
2016-10-12 17:17:26 +0300

    5.2.9 done

--- src/org/stepic/java/week5/lesson2/task9/Main.java
+++ src/org/stepic/java/week5/lesson2/task9/Main.java
@@ -12,35 +12,41 @@ import java.util.Arrays;
 Данные в формате Windows подаются программе в System.in, преобразованные данные должны выводиться в
 System.out. На этот раз вам надо написать программу полностью, т.е. объявить класс (с именем Main —
 таково ограничение проверяющей системы), метод main, прописать все import'ы.
+
+\r == 13
+\n == 10
+
  */
 public class Main {
     public static void main(String[] args) {
-        int ch, bufferedCh;
+        int ch, buffer;
         try (
-                InputStream inStream = new ByteArrayInputStream(new byte[]{99, 13, 13, 10, 13,99});
-                //InputStream inStream = System.in;
-                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-                //OutputStream outStream = System.out;
+                //InputStream inStream = new ByteArrayInputStream(new byte[]{13,10,99,13,10});
+                InputStream inStream = System.in;
+                //ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                OutputStream outStream = System.out;
 
             ){
-            bufferedCh = inStream.read();
-            if (bufferedCh == -1) {
+
+            buffer = inStream.read();
+
+            if (buffer == -1) {
                 System.exit(0); //пустой поток
             }
             while ( (ch = inStream.read()) != -1 ){
-                if ( (bufferedCh == 13) && (ch == 10)) {// \r \n
-                    bufferedCh = ch;
+                if ( (buffer == 13) && (ch == 10)) {// \r \n
+                    buffer = ch;
                     continue;
                 }
-                outStream.write(bufferedCh);
-                bufferedCh = ch;
+                outStream.write(buffer);
+                buffer = ch;
             }
 
-            outStream.write(bufferedCh);
+            outStream.write(buffer);
 
             outStream.flush();
-            byte[] res = outStream.toByteArray();
-            System.out.println(Arrays.toString(res));
+            //byte[] res = outStream.toByteArray();
+            //System.out.println(Arrays.toString(res));
         } catch (IOException ioe){ /* на самом деле не ловим */ }
     }
 }
