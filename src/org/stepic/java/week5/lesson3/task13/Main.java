b46b7b8b4146642a383edc06bccb78f1a3eb3545
railprophet <rbdmit@gmail.com>
2016-10-12 17:40:48 +0300

    5.3.13 done

--- src/org/stepic/java/week5/lesson3/task13/Main.java
+++ src/org/stepic/java/week5/lesson3/task13/Main.java
@@ -1,14 +1,29 @@
 package org.stepic.java.week5.lesson3.task13;
 
+/*
+Напишите программу, читающую текст из System.in и выводящую в System.out сумму всех встреченных в тексте вещественных
+чисел с точностью до шестого знака после запятой. Числом считается последовательность символов, отделенная от
+окружающего текста пробелами или переводами строк и успешно разбираемая методом Double.parseDouble.
+
+ */
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double sum=0;
-        String str;
-        while (scan.hasNextDouble()) {
-            sum += scan.nextDouble();
+
+        while (scan.hasNext()) {
+            try {
+                sum += Double.parseDouble(scan.next());
+            } catch (java.lang.NumberFormatException e){}
+            //System.out.println(sum);
         }
+        System.out.format("%.6f", sum);
+
     }
 }
